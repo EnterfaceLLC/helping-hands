b6a1cf224ddbb2692f7b5435657dd0383bb97f3a
EnterfaceLLC <dotenterface@gmail.com>
2024-08-28 17:10:59 -0500

    +Clerk Auth Config

--- app/Login/index.tsx
+++ app/Login/index.tsx
@@ -1,8 +1,46 @@
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking";
 import { View, Text, Image, Pressable } from "react-native";
-import React from "react";
+import React, { useCallback } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+//* LOGIN SCREEN //
+export const useWarmUpBrowser = () => {
+  React.useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
+
 const Login = () => {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow({
+          redirectUrl: Linking.createURL("/home", { scheme: "myapp" }),
+        });
+
+      if (createdSessionId) {
+        // setActive!({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FBFEE9" }}>
       <Text
@@ -21,11 +59,11 @@ const Login = () => {
         style={{ width: "100%", height: 400 }}
       />
       <View
-      style={{
-        flex: 1,
-        padding: 10,
-        justifyContent: 'space-between',
-      }}
+        style={{
+          flex: 1,
+          padding: 25,
+          justifyContent: "space-between",
+        }}
       >
         <Text
           style={{
@@ -49,10 +87,11 @@ const Login = () => {
           families
         </Text>
         <Pressable
+          onPress={onPress}
           style={{
             width: "100%",
             borderRadius: 30,
-            alignSelf: 'center',
+            alignSelf: "center",
             backgroundColor: "#82C1D2",
             padding: 15,
           }}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,41 @@
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+
+//* APP ROOT LAYOUT//
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key)
+      if (item) {
+        console.log(`${key} was used 🔐 \n`)
+      } else {
+        console.log('No values stored under key: ' + key)
+      }
+      return item
+    } catch (error) {
+      console.error('SecureStore get item error: ', error)
+      await SecureStore.deleteItemAsync(key)
+      return null
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      return
+    }
+  },
+}
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
 
 export default function RootLayout() {
   useFonts({
@@ -7,9 +43,11 @@ export default function RootLayout() {
   });
 
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="Login/index" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="Login/index" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
   );
 }
